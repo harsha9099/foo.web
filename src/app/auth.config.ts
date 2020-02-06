68583c5710fb2781e21edf2dc812a882eddc2878
Jagtar <jagtar.singh@lngconsultancy.co.in>
2020-02-06 18:06:42 +0530

    custom policy added

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -1,7 +1,7 @@
 import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const DiscoveryDocumentConfig = {
-  url : "https://idmcc.b2clogin.com/idmcc.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1_cc"
+  url : "https://idmcc.b2clogin.com/idmcc.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1A_signup_signin"
 }
 
 export const authConfig: AuthConfig = {
@@ -9,8 +9,8 @@ export const authConfig: AuthConfig = {
   responseType: 'token id_token',
   issuer: 'https://idmcc.b2clogin.com/b6336d91-19ac-4844-9585-f53f71498c49/v2.0/',
   strictDiscoveryDocumentValidation: false,
-  tokenEndpoint: 'https://idmcc.b2clogin.com/idmcc.onmicrosoft.com/oauth2/v2.0/token?p=b2c_1_cc',
-  loginUrl: 'https://idmcc.b2clogin.com/idmcc.onmicrosoft.com/oauth2/v2.0/token?p=b2c_1_cc',
+  tokenEndpoint: 'https://idmcc.b2clogin.com/idmcc.onmicrosoft.com/oauth2/v2.0/token?p=B2C_1A_signup_signin',
+  loginUrl: 'https://idmcc.b2clogin.com/idmcc.onmicrosoft.com/oauth2/v2.0/token?p=B2C_1A_signup_signin',
   clientId: 'e4dcbad6-4500-47f8-88ec-fafe40f6fdff',
   scope: 'openid profile https://idmcc.onmicrosoft.com/credit-coach-ad/user_impersonation',
   skipIssuerCheck: true,
